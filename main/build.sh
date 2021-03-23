97857f7ceb6ce225a45acd346d4c004ef7789119
Alex Athanasopoulos <alex@melato.org>
2021-03-23 18:37:27 +0200

    removed demo, test, old things

--- main/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# compile a go program statically, so that it can run in any container,
-# for example, in alpine containers
-mkversion -t version.tpl version.go
-CGO_ENABLED=0 go install -ldflags '-extldflags "-static"' lxdops.go version.go
